d45e968ecddbf8f8268ad9a4a73b5fdaa0f44591
DenDen047 <sh.mn.nat@gmail.com>
2022-08-19 10:48:50 +0200

    Setup Sources

--- setup_scripts/ros2_foxy.sh
+++ setup_scripts/ros2_foxy.sh
@@ -9,3 +9,6 @@ sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 
 # Setup Sources
+sudo apt update && sudo apt install curl gnupg2 lsb-release -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
+
