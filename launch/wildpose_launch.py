a0599fa9cd8ec5afb6d651a9de9a6a3b8462a2ef
DenDen047 <sh.mn.nat@gmail.com>
2022-08-29 15:11:19 +0200

    update the launch for ximea camera

--- launch/wildpose_launch.py
+++ launch/wildpose_launch.py
@@ -4,7 +4,10 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 import launch
 
-################### user configure parameters for ros2 start ###################
+import yaml
+
+
+################### Livox TELE-15 user-defined parameters start ###################
 xfer_format   = 0    # 0-Pointcloud2(PointXYZRTL), 1-customized pointcloud format
 multi_topic   = 0    # 0-All LiDARs share the same topic, 1-One LiDAR one topic
 data_src      = 0    # 0-lidar,1-hub
@@ -18,7 +21,7 @@ cur_path = os.path.split(os.path.realpath(__file__))[0] + '/'
 cur_config_path = cur_path + '../config'
 rviz_config_path = os.path.join(cur_config_path, 'livox_lidar.rviz')
 user_config_path = os.path.join(cur_config_path, 'livox_lidar_config.json')
-################### user configure parameters for ros2 end #####################
+################### Livox TELE-15 user-defined parameters end #####################
 
 livox_ros2_params = [
     {"xfer_format": xfer_format},
@@ -32,7 +35,97 @@ livox_ros2_params = [
     {"cmdline_input_bd_code": cmdline_bd_code}
 ]
 
+
+ximea_cam_parameters = {
+    ################### XIMEA camera user-defined parameters start ###################
+    ####################
+    # General Configuration Parameters Go Here!
+    ####################
+
+    # camera properties (https://github.com/wavelab/ximea_ros_cam)
+    'serial_no': "29970951",  # serial number on the backplate
+    'cam_name': "ximea_MQ022CG-CM",   # Name of the camera used when saving camera images and snapshots under the directory pointed by image_directory
+    'calib_file': "", # Calibration file used by the camera
+    'frame_id': '0',
+    'num_cams_in_bus': 1, # Number of USB cameras processed by a single USB controller
+    'bw_safetyratio': 1.0,    # Bandwidth safety ratio, a multiplier to the bandwidth allocated for each camera
+    'poll_time': 2.0, # Used to set the duration (in seconds) which the camera is attempted to be opened again.
+    'poll_time_frame': 1/100, # the ROS timer loop period (in seconds) for the ximea camera node. It should generally be set to a rate that is a factor higher than the camera capture rate.
+    'publish_xi_image_info': True,    # Flag for publishing the extra ximea camera information provided with each image acquisition.
+
+    # directory to save images (make sure that directory exists and that it is an absolute path).
+    'image_directory': "/home/dev/",    # must be absolute path, not relative path (i.e. '~')
+
+    # save images to disk, under the directory `<image_directory>/stream`
+    'save_disk': False,
+
+    # Saves images everytime a trigger is pressed, under the director `<image_directory>/calib`
+    'calib_mode': False,
+
+    ####################
+    # Diagnostics Configuration Parameters Go Here!
+    ####################
+
+    'enable_diagnostics': True,
+    'data_age_max': 0.1,
+    'pub_frequency': 10.0,
+    'pub_frequency_tolerance': 1.0,
+
+    ####################
+    # Camera Configuration Parameters Go Here!
+    ####################
+
+    # image_transport compressed image parameters
+    'image_transport_compressed_format': "png", # jpg or png
+    'image_transport_compressed_jpeg_quality': 100, # 1 to 100 (1: min quality)
+    'image_transport_compressed_png_level': 5,  # 1 to 9 (9: max compression)
+
+    # colour image format
+    'format': "XI_RGB24", # BGR 24 bit
+
+    # camera coloring
+    # white balance mode: 0 - none, 1 - use coefficients, 2: auto
+    'white_balance_mode': 2,
+    'white_balance_coef_red': 3.0,  # white balance red coefficient (0 to 8)
+    'white_balance_coef_green': 0.0,    # white balance green coefficient (0 to 8)
+    'white_balance_coef_blue': 4.0, # white balance blue coefficient (0 to 8)
+
+    # triggering (0 - none, 1 - software trigger (NOT IMPLMENTED YET), 2 - hardware trigger)
+    'cam_trigger_mode': 0,
+    'hw_trigger_edge': 0,   # if hw trigger, 0/1: rising/falling edge trigger
+
+    # for camera frame rate
+    'frame_rate_control': True,  # enable or disable frame rate control (works if no triggering is enabled)
+    'frame_rate_set': 0, # for trigger mode, fps limiter (0 for none)
+    'img_capture_timeout': 1000,    # timeout in milliseconds for xiGetImage()
+
+    # exposure settings
+    'auto_exposure': False,          # auto exposure on or off
+    'exposure_time': 6000,           # manual exposure time in microseconds
+    'manual_gain': 9.0,              # manual exposure gain
+    'auto_exposure_priority': 0.8,   # auto exposure to gain ratio (1: use only exposure)
+    'auto_time_limit': 30000,        # auto exposure time limit in microseconds
+    'auto_gain_limit': 2.0,          # auto exposure gain limit
+
+    # region of interest
+    'roi_left': 0,      # top left corner in pixels
+    'roi_top': 0,
+    'roi_width': 1280,  # width height in pixels
+    'roi_height': 1024,
+    ################### XIMEA camera user-defined parameters end #####################
+}
+
+ximea_ros2_cam_params = [{k: v} for k, v in ximea_cam_parameters.items()]
+
 def generate_launch_description():
+    ximea_cam_driver = Node(
+        package='ximea_ros2_cam',
+        executable='ximea_ros2_cam_node',
+        name='ximea_cam_publisher',
+        output='screen',
+        parameters=ximea_ros2_cam_params
+    )
+
     livox_driver = Node(
         package='livox_ros2_driver',
         executable='livox_ros2_driver_node',
@@ -42,5 +135,5 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        livox_driver
+        ximea_cam_driver,
     ])
