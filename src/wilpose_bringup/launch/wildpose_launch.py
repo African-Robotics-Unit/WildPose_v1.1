c41d6d09bf59058559f7a9585dc93ad232852477
Naoya Muramatsu <sh.mn.nat@gmail.com>
2022-09-05 07:45:07 +0200

    prepare the launch file

--- src/wilpose_bringup/launch/wildpose_launch.py
+++ src/wilpose_bringup/launch/wildpose_launch.py
@@ -2,9 +2,6 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-import launch
-
-import yaml
 
 
 ################### Livox TELE-15 user-defined parameters start ###################
