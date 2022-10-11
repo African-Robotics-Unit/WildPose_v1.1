ad8591fa5609600f4e36b6520164066b70584909
Naoya Muramatsu <sh.mn.nat@gmail.com>
2022-10-11 12:16:05 +0200

    update the can setting

--- src/dji_rs3_pkg/enable_CAN.sh
+++ src/dji_rs3_pkg/enable_CAN.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+# https://docs.nvidia.com/jetson/archives/r34.1/DeveloperGuide/text/HR/ControllerAreaNetworkCan.html
 
-sudo busybox devmem 0x0c303000 32 0x0000C400
-sudo busybox devmem 0x0c303008 32 0x0000C458
-sudo busybox devmem 0x0c303010 32 0x0000C400
-sudo busybox devmem 0x0c303018 32 0x0000C458
+sudo busybox devmem 0x0c303010 w 0x400
+sudo busybox devmem 0x0c303018 w 0x458
 
 sudo modprobe can
 sudo modprobe can_raw
@@ -11,5 +10,8 @@ sudo modprobe mttcan
 
 sudo ip link set can0 type can bitrate 1000000 dbitrate 1000000 berr-reporting on fd on
 sudo ip link set up can0
+
+# sudo ip link set down can0
+
 
 exit 0
