16f331d4dedb0becf29de472c7cec7d8c547ff45
Naoya Muramatsu <sh.mn.nat@gmail.com>
2022-10-12 11:25:46 +0200

    removed redundant files

--- src/gamepad_pkg/setup.py
+++ src/gamepad_pkg/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "f710_node = gamepad_pkg.f710_node:main",
+            # "f710_node = gamepad_pkg.f710_node:main",
         ],
     },
 )
